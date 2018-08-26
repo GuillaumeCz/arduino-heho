3e23aac06006239a3e31c7b82c7ede163e506d7e
GuillaumeCz <guillaume.couzy@gmail.com>
2018-08-26 17:39:30 +0200

    Add multiplexer and remove delay onChange

--- distance/distance.ino
+++ distance/distance.ino
@@ -1,75 +1,63 @@
 #include <SR04.h>
 
-//www.elegoo.com
-//2016.12.8
-
-// Led Pins
-// RED
-#define RED0 2
-#define RED1 3
-#define RED2 4
-#define RED3 5
-#define RED4 6
-
-// GREEN
-#define GREEN0 7
-#define GREEN1 8
-#define GREEN2 9
-#define GREEN3 10
-#define GREEN4 13
-
 // Ultrasonic Sensor's pins
 #define TRIG 11
 #define ECHO 12
 
 #define DELAYTIME 500// fading time between colors
 
-// Arrays of pins
-int redPins[5] = {RED0, RED1, RED2, RED3, RED4};
-int greenPins[5] = {GREEN0, GREEN1, GREEN2, GREEN3, GREEN4};
-int allLedPins[10] = {RED0, RED1, RED2, RED3, RED4, GREEN0, GREEN1, GREEN2, GREEN3, GREEN4};
-
 // input of sensor
 SR04 sr04 = SR04(ECHO, TRIG);
 
+int latchPin = 6;
+int clockPin = 5;
+int dataPin = 7;
+
+byte leds = 0;
+
+int lastLed = 0;
+
 // variables to calcultate the thresholds
 int distMax = 150;
-int nbrLed = 10;
+int nbrLed = 8;
 int pas = distMax/nbrLed;
 
 void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < 10; i++) {
-    pinMode(allLedPins[i], OUTPUT); 
-  }
+
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
 }
 
 void shutAll() {
-  for(int i = 0 ; i < 10; i++) {
-     digitalWrite(allLedPins[i], LOW);  
-  }  
+  leds = 0;
+  updateShiftRegister();
 }
 
 void ledOn(int _led) {
-  digitalWrite(allLedPins[_led], HIGH);  
-}
-
-void ledOff(int _led) {
-  digitalWrite(allLedPins[_led], LOW);  
+  if(lastLed != _led){
+    lastLed = _led;
+    shutAll();  
+  } else {
+    bitSet(leds, _led);
+    updateShiftRegister();
+  }
 }
 
 // main loop
 void loop() {
-   long distance = sr04.Distance();
-   
-   for(int i = 0; i < 10; i++) {
-      if(distance <= pas * i && distance > (pas * i) - pas) {
-        ledOn(i);
-        delay(DELAYTIME);
-        ledOff(i);
-        delay(DELAYTIME);
-      }   
-   }
-   
+  long distance = sr04.Distance();
+  for(int i = 0 ; i< 8; i++) { 
+    if(distance <= pas * i && distance > (pas * i) -pas) {
+      ledOn(i);
+    } 
+  }
 }
+
+void updateShiftRegister() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
+  }
 
